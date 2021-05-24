e6e25931db7a2da02bbc95073ca5ff84b61b002e
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-05-24 12:40:34 +0300

    W05D02

---
+++ CarsStore/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsStore.Controllers
+{
+    public class CarsController : Controller
+    {
+        public string Index()
+        {
+            return "test";
+        }
+
+        public IActionResult Details()
+        {
+            ViewData["Name"] = "BMW";
+            ViewData["Model"] = 2020;
+            ViewData["Color"] = "Black";
+
+            return View();
+        }
+
+        public IActionResult Type()
+        {
+            return View();
+        }
+    }
+}
